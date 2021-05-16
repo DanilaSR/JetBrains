df63b4f09940227514900f7af15d9709b3850347
DANExtraZero <72612144+DANExtraZero@users.noreply.github.com>
2021-05-16 12:35:36 +0300

    Create cmake_clean.cmake

---
+++ cmake-build-debug/CMakeFiles/JetBrains.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JetBrains.dir/main.cpp.obj"
+  "JetBrains.exe"
+  "JetBrains.exe.manifest"
+  "JetBrains.pdb"
+  "libJetBrains.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JetBrains.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
